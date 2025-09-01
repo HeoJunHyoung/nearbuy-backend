311985a8065d4fa25863bb4c47522cd25594effe
junbro <gjwnsgud4016@gmail.com>
2025-09-01 15:09:03 +0900

    refactor: Exception 분기 처리

--- src/main/java/io/github/junhyoung/nearbuy/global/exception/GlobalControllerAdvice.java
+++ src/main/java/io/github/junhyoung/nearbuy/global/exception/GlobalControllerAdvice.java
@@ -1,26 +1,50 @@
 package io.github.junhyoung.nearbuy.global.exception;
 
+import io.github.junhyoung.nearbuy.global.exception.business.InvalidPasswordException;
+import io.github.junhyoung.nearbuy.global.exception.business.PostNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalControllerAdvice {
 
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<String> handleUsernameNotFoundException(UsernameNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(PostNotFoundException.class)
+    public ResponseEntity<String> handlePostNotFoundException(PostNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(InvalidPasswordException.class)
+    public ResponseEntity<String> handleInvalidPasswordException(InvalidPasswordException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
+
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<String> handleAccessDeniedException(AccessDeniedException ex) {
         return ResponseEntity
                 .status(HttpStatus.FORBIDDEN)
-                .body("접근 권한이 없습니다.");
+                .body(ex.getMessage());
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body("잘못된 요청입니다.");
+                .body(ex.getMessage());
     }
 
 }
---
+++ src/main/java/io/github/junhyoung/nearbuy/global/exception/business/PostNotFoundException.java
@@ -0,0 +1,18 @@
+package io.github.junhyoung.nearbuy.global.exception.business;
+
+public class PostNotFoundException extends RuntimeException{
+    public PostNotFoundException() {
+    }
+
+    public PostNotFoundException(String message) {
+        super(message);
+    }
+
+    public PostNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PostNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
